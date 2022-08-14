57835ca4372b00ece6e636eefdb6872fe5a9c411
MohdSajid12 <108975916+MohdSajid12@users.noreply.github.com>
2022-08-14 21:06:59 +0530

    Add files via upload

---
+++ PROJECTS/base-project/index.js
@@ -0,0 +1,10 @@
+const bttt = document.getElementById("btn")
+
+const nav = document.getElementById("nav")
+
+
+bttt.addEventListener("click" , () =>{
+    nav.classList.toggle("active");
+    btn.classList.toggle("active");
+
+});
